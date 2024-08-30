7958f7c6d80b6a38251e29c2620dce5a96b1d9df
davv3hustla <davvee_9@hotmail.com>
2024-08-30 10:41:08 +0200

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program begin");
+
+        int sum = 0;
+
+        // Loop för att beräkna summan
+        for (int i = 0; i < 4; i++)
+        {
+            sum += i; // Lägg till värdet av i till summan
+            Console.WriteLine($"i = {i}");
+            Console.WriteLine($"sum = {sum}");
+
+            // Särskilt meddelande när i = 2
+            if (i == 2)
+            {
+                Console.WriteLine("We have now the magical sum of 3");
+            }
+        }
+    }
+}
