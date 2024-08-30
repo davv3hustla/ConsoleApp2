3181002a05abfb50b60ef6b1b9278d33d958f4c7
davv3hustla <davvee_9@hotmail.com>
2024-08-30 11:51:10 +0200

    CommentsUpdate

--- Program.cs
+++ Program.cs
@@ -2,25 +2,30 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{      
+ 
+ static void loop(int nr){ /* static (don't worry about it), void means the method does not return a value, loop is the name*/
+        /* int nr, means we import a value (from the code that jumps here), and calls it nr, this is the number of loops we will execute */
+        int sum = 0; /* the running sum to calculate */
+        for (int i = 0; i < nr; i++)
+        { /* For 0 to 'nr' (couting with the variable i) (i++ -> increment i by one) */
+                Console.WriteLine($"i = {i}"); /* Print "i = x" and replace x by the value of i */
+                if((i % 2) == 1) { /* Only executes when i is odd */
+                        sum = sum + i; /* update the sum with the value of i, but only if i is odd */
+                }
+                Console.WriteLine($"sum = {sum}"); /* same as i = x, but with sum */
+                if(sum == 3){ /* Execute the code below when sum equals 3 */
+                        Console.WriteLine("We have now the magical sum of 3");
+                }
+               
+        }
+ }
+ static void Main()
     {
         Console.WriteLine("Program begin");
-
-        int sum = 0;
-
-        // Loop för att beräkna summan
-        for (int i = 0; i < 4; i++)
-        {
-            sum += i; // Lägg till värdet av i till summan
-            Console.WriteLine($"i = {i}");
-            Console.WriteLine($"sum = {sum}");
-
-            // Särskilt meddelande när i = 2
-            if (i == 2)
-            {
-                Console.WriteLine("We have now the magical sum of 3");
-            }
-        }
+        loop(11); /* Execute the loop-method with 11 as the parameter (named nr in the method) */
+        Console.WriteLine("Program ends");
     }
 }
+ 
+ 
--- obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7958f7c6d80b6a38251e29c2620dce5a96b1d9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
